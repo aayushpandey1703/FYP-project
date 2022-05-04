ff400a60d08ddfd02463eac42687cf85697620a3
Edgy Eft <48384216+aayushpandey1703@users.noreply.github.com>
2022-05-04 14:29:45 +0530

    Update main.py

--- Colab/main.py
+++ Colab/main.py
@@ -26,7 +26,8 @@ access_token='1287085799339462656-yMJ0c9NzCgbwu7sd9QEVjyXUKkWieS'
 access_token_secret='OPq0IiDTyGmhziS0EUNyYbupdr492tuB848rtGT8BxzLS'
 
 #load model
-file_to_read = open("./stored_object.pickle", "rb")
+with open('stored_object.pickle', 'rb') as f:
+    file_to_read = pickle.load(f)
 z=pickle.load(file_to_read)
 t=Tokenizer(num_words=50000,lower=True)
 t.fit_on_texts(z)

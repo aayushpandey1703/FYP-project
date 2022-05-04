8e5b45f55cf09dc94c2acec00bef75f9574832e6
Naresh192 <35963618+Naresh192@users.noreply.github.com>
2022-05-04 21:43:52 +0530

    Update main.py

--- Colab/main.py
+++ Colab/main.py
@@ -274,7 +274,7 @@ colx1,colx2,colx3,colx4=st.columns(4)
 if option=='iphone 11': 
   colx1.image("https://media.wired.com/photos/5bcea2642eea7906bba84c67/master/w_2560%2Cc_limit/iphonexr.jpg",width=300)
 else :
-  colx1.image("https://www.remove.bg/e6b6779a-c005-46e5-adff-58c0182e0f8f",width=300)
+  colx1.image("https://i.ibb.co/7pH96cC/71-IX75jo-Wj-L-AC-SL1500-removebg.png",width=300)
 
 col2.title("Data Frame")
 colx3.write(tweetDf[['username','tweet','location']])

3f368411879efcb8903026090ddf71964c6638c8
Edgy Eft <48384216+aayushpandey1703@users.noreply.github.com>
2022-05-05 08:55:55 +0530

    Update main.py

--- Colab/main.py
+++ Colab/main.py
@@ -277,7 +277,7 @@ else :
   colx1.image("https://i.ibb.co/7pH96cC/71-IX75jo-Wj-L-AC-SL1500-removebg.png",width=300)
 
 col2.title("Scrapped Tweets")
-col3.title("Positive Tweets")
+colx4.title("Positive Tweets")
 colx3.write(tweetDf[['username','tweet','location']])
 colx4.write(k)
 
